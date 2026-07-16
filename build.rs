206470042b9621ae9b60b10ea5312a9ac243c7ef
vikrinox <viktor@koda.re>
2026-07-16 17:33:24 +0200

    desperation strikes

--- build.rs
+++ build.rs
@@ -3,16 +3,19 @@ use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut builder = cc::Build::new();
-    let builder = builder
-        .file("fatfs/source/ff.c");
-        
-    builder.compile("fatfs");
+    let _builder = builder
+        .file("fatfs/source/ff.c")
+        //.file("fatfs/source/ffunicode.c")
+        .target("thumbv5te-none-eabi")
+        .compiler("arm-none-eabi-gcc")
+        .flag("-Oz")
+        .compile("fatfs");
 
-    let target = env::var("TARGET")?;
+    let _target = env::var("TARGET")?;
 
     let bindings = bindgen::Builder::default()
         .header("fatfs/source/ff.h")
-        .clang_arg(format!("--target={}", target))
+        .clang_arg(format!("--target=thumbv5te-none-eabi"))
         .use_core()
         .ctypes_prefix("cty")
         .derive_copy(false)
@@ -23,6 +26,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
-
     Ok(())
 }

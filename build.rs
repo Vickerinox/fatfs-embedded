daced446f686163568de1d18992758749275e6b4
Vikrinox <viktor@koda.re>
2026-07-24 19:29:55 +0200

    document build script better

--- build.rs
+++ build.rs
@@ -5,6 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     let target = env::var("TARGET")?;
 
+    // Generate the bindings for fatfs
     let bindings = bindgen::Builder::default()
         .header("fatfs/source/ff.h")
         .clang_arg(format!("--target={}", target))
@@ -14,20 +15,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .generate()
         .expect("Unable to generate bindings");
 
+    // Add them to our rust sourcecode
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
 
+
+    // Building fatfs into our project
     let mut builder = cc::Build::new();
     
     // My host machine keeps f'ing this up, so lets try this instead.
-    // This tries building with whatever cc picks
+    // This tries building with whatever cc picks, which should work on most setups. 
+    // (NOTE: if we tried setting an explicit target, eg. rust-analyzer may not work correctly)
     let build_result = builder
         .file("fatfs/source/ff.c")
         .try_compile("fatfs");
 
-    // If it fails, use the expected arm embedded compiler instead.
+    // If default build fails, use the expected arm embedded compiler instead.
+    // (which is what the build program for astronaut expects.)
     if build_result.is_err() {
         builder
             .file("fatfs/source/ff.c")

c32fb98dd07ad1316a89fb336a37eb09bda3f58e
Vikrinox <viktor@koda.re>
2026-07-24 18:53:11 +0200

    compiler fix

--- build.rs
+++ build.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut builder = cc::Build::new();
     let builder = builder
         .file("fatfs/source/ff.c")
-        .compiler("gcc-arm-none-eabi");
+        .compiler("arm-none-eabi-gcc");
         
     builder.compile("fatfs");
 
